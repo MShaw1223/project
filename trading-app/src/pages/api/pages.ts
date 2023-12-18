fb043a8dda145899155ad5d3d7b9fa5faa4e2d16
miller <millershaw61@gmail.com>
2023-12-18 11:12:47 +0000

    pages edits

--- trading-app/src/pages/api/pages.ts
+++ trading-app/src/pages/api/pages.ts
@@ -1,17 +1,65 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextFetchEvent, NextRequest } from 'next/server';
+import { Pool } from '@neondatabase/serverless';
 
 export const config = {
   runtime: 'edge',
 };
 
-type Data = {
-  name: string
+async function extractBody(req: NextRequest) {
+  if(!req.body){
+    return null;
+  }
+
+  const decoder = new TextDecoder();
+
+  const reader = req.body.getReader();
+
+  let body = "";
+
+  while(true){
+    const {done, value} = await reader.read();
+    if(done){
+      try{
+        return JSON.parse(body)
+      }catch(e){
+        console.error(e)
+        return null
+      };
+    }
+    body = body + decoder.decode(value)
+
+  }
+
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+async function createPageHandler(req: NextRequest, event: NextFetchEvent) {
+  const body = req.body
+  
+  console.log('body', body)
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+
+  const sql = 'SELECT NOW()'
+  
+  const {rows} = await pool.query(sql)
+  
+  const now = rows[0]
+  
+  event.waitUntil(pool.end());
+  
+  return new Response(JSON.stringify({ now }),{
+    status: 200
+  });
+}
+
+export default async function handler(req: NextRequest, event: NextFetchEvent) {
+  if(req.method === 'POST'){
+    return createPageHandler(req, event)
+  }
+  return new Response("Invalid Method",{
+    status: 405
+  });
 }
