81540a551566ba5e09e260cd9506e5518f02082f
miller <millershaw61@gmail.com>
2023-12-21 22:04:43 +0000

    changes: maybe allow use on phone (j playing abt)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head/>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>
           NEA Project
         </title>
