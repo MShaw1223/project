8d9f117b39d46f9c257e0b6bf494a74f806128e8
miller <millershaw61@gmail.com>
2023-12-25 22:08:20 +0000

    changes from prettier

--- src/pages/Trade-Entry.tsx
+++ src/pages/Trade-Entry.tsx
@@ -1,52 +1,56 @@
 import { AccountDropdown } from "@/components/ui/selectAccount";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useMutation } from "react-query"
+import { useMutation } from "react-query";
 import { useState, FormEvent } from "react";
 import Link from "next/link";
 import { FaHome } from "react-icons/fa";
 import { NextPage } from "next";
 
-
 const tradeEntry: NextPage = () => {
-  const [selectedAccount, setSelectedAccount] = useState<string>('')
-  
+  const [selectedAccount, setSelectedAccount] = useState<string>("");
+
   const mutation = useMutation({
     mutationFn: async (formData: string) => {
-      const response = await fetch("/api/entries",{
+      const response = await fetch("/api/entries", {
         method: "POST",
         body: formData,
-        cache: 'no-store'
-      })
+        cache: "no-store",
+      });
       if (!response.ok) {
-        throw new Error('Failed to submit trade data');
+        throw new Error("Failed to submit trade data");
       }
-      
+
       return response.json();
     },
     onSettled: () => {
       // Reset any state related to the mutation
-      setSelectedAccount('');
+      setSelectedAccount("");
     },
     onError: (error) => {
-      console.error('Mutation error:', error);
+      console.error("Mutation error:", error);
     },
-  })
-  
+  });
+
   const handleAccountChange = (selectedAccount: string) => {
     setSelectedAccount(selectedAccount);
   };
-  
-  function handleSubmit(event: FormEvent<HTMLFormElement>){
-    event.preventDefault()
-    
-    const data = new FormData(event.target as HTMLFormElement)
-    const entryPrice = parseFloat(data.get("entryPrice")as string);
-    const stopLoss = parseFloat(data.get("stopLoss")as string);
-    const takeProfit = parseFloat(data.get("takeProfit")as string);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.target as HTMLFormElement);
+    const entryPrice = parseFloat(data.get("entryPrice") as string);
+    const stopLoss = parseFloat(data.get("stopLoss") as string);
+    const takeProfit = parseFloat(data.get("takeProfit") as string);
     const selectedAccountValue = selectedAccount;
-    
-    if(!entryPrice || !stopLoss || !takeProfit || selectedAccountValue === ''){
+
+    if (
+      !entryPrice ||
+      !stopLoss ||
+      !takeProfit ||
+      selectedAccountValue === ""
+    ) {
       alert("Invalid entry");
       return;
     }
@@ -56,51 +60,68 @@ const tradeEntry: NextPage = () => {
       takeProfit,
       selectedAccount: selectedAccountValue,
     });
-    
+
     mutation.mutate(requestData);
   }
-  
-  
+
   return (
     <main className="text-center p-3 bg-slate-200 h-screen">
       <div className="flex flex-col items-center">
-        <h1 className="m-4 text-4xl font-extrabold text-black">
-          Trade Entry
-        </h1>
+        <h1 className="m-4 text-4xl font-extrabold text-black">Trade Entry</h1>
         <div className="p-3">
-            <Link href="/home">
-              <FaHome className="w-7 h-7"></FaHome>
-            </Link>
+          <Link href="/home">
+            <FaHome className="w-7 h-7"></FaHome>
+          </Link>
         </div>
       </div>
       {mutation.isLoading && <p>Submitting Trade Data...</p>}
       {!mutation.isLoading && (
-      <div className="flex flex-col items-center">
-        <div>
-          <AccountDropdown onAccountChange={handleAccountChange}></AccountDropdown>
-        </div>
-        <div>
+        <div className="flex flex-col items-center">
           <div>
-            <form className="my-auto w-80" onSubmit={handleSubmit}>
-              <div className="p-3">
-                <Input id="entryPrice" name="entryPrice" type="number" step="any" placeholder="Entry Price..."></Input>
-              </div>
-              <div className="p-3">
-                <Input id="stopLoss" name="stopLoss" type="number" step="any" placeholder="Stop Loss..."></Input>
-              </div>
-              <div className="p-3">
-                <Input id="takeProfit" name="takeProfit" type="number" step="any" placeholder="Take Profit..."></Input>
-              </div>
-              <div className="p-3">
-                <Button type="submit">Submit Entry</Button>
-              </div>
-            </form>
+            <AccountDropdown
+              onAccountChange={handleAccountChange}
+            ></AccountDropdown>
+          </div>
+          <div>
+            <div>
+              <form className="my-auto w-80" onSubmit={handleSubmit}>
+                <div className="p-3">
+                  <Input
+                    id="entryPrice"
+                    name="entryPrice"
+                    type="number"
+                    step="any"
+                    placeholder="Entry Price..."
+                  ></Input>
+                </div>
+                <div className="p-3">
+                  <Input
+                    id="stopLoss"
+                    name="stopLoss"
+                    type="number"
+                    step="any"
+                    placeholder="Stop Loss..."
+                  ></Input>
+                </div>
+                <div className="p-3">
+                  <Input
+                    id="takeProfit"
+                    name="takeProfit"
+                    type="number"
+                    step="any"
+                    placeholder="Take Profit..."
+                  ></Input>
+                </div>
+                <div className="p-3">
+                  <Button type="submit">Submit Entry</Button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-    </div>    
       )}
     </main>
-  )
-}
+  );
+};
 
 export default tradeEntry;
