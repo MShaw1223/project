2a484e4bff1287b9124416cf9219f99f85bda99e
miller <millershaw61@gmail.com>
2024-01-04 23:48:35 +0000

    added seperator and made menu look better

--- src/utils/menu.tsx
+++ src/utils/menu.tsx
@@ -4,97 +4,81 @@ import { MdOutlineAccountBalance, MdOutlineManageSearch } from "react-icons/md";
 import { BsFillJournalBookmarkFill } from "react-icons/bs";
 import { MdArrowBack } from "react-icons/md";
 import { useRouter } from "next/router";
+import { Separator } from "@/components/ui/separator";
 
 export default function Menu() {
   const router = useRouter();
   const isLinkActive = (href: string) => router.pathname === href;
   return (
-    <div className="w-80 h-screen bg-slate-500">
+    <div className="w-80 h-screen bg-slate-400">
       <div className="text-center p-5 text-2xl font-extrabold">Menu</div>
-
       <Link href="/home">
         <div
-          className={`flex pl-8 py-8 focus:outline-none ${
-            isLinkActive("/home")
-              ? "bg-gray-700 font-black  text-xl"
-              : "font-bold"
-          }  hover:underline underline-offset-8`}
+          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
+            isLinkActive("/home") ? "bg-slate-600 hover:bg-slate-600" : ""
+          } rounded-full `}
         >
-          <FaHome
-            className={isLinkActive("/home") ? "w-9 h-9" : "w-6 h-6"}
-          ></FaHome>
+          <FaHome className="w-8 h-8"></FaHome>
           <span className="ml-12 my-auto">Home</span>
         </div>
       </Link>
 
       <Link href="/Trade-Entry">
         <div
-          className={`flex pl-8 py-8 focus:outline-none ${
+          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
             isLinkActive("/Trade-Entry")
-              ? "bg-gray-700 font-black  text-xl"
-              : "font-bold"
-          }  hover:underline underline-offset-8`}
+              ? "bg-slate-600 hover:bg-slate-600"
+              : ""
+          } rounded-full `}
         >
-          <BsFillJournalBookmarkFill
-            className={isLinkActive("/Trade-Entry") ? "w-9 h-9" : "w-6 h-6"}
-          ></BsFillJournalBookmarkFill>
+          <BsFillJournalBookmarkFill className="w-8 h-8"></BsFillJournalBookmarkFill>
           <span className="ml-12 my-auto">Trade Entry</span>
         </div>
       </Link>
 
       <Link href="/entry-management">
         <div
-          className={`flex pl-8 py-8 focus:outline-none ${
+          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
             isLinkActive("/entry-management")
-              ? "bg-gray-700 font-black  text-xl"
-              : "font-bold"
-          }  hover:underline underline-offset-8`}
+              ? "bg-slate-600 hover:bg-slate-600"
+              : ""
+          } rounded-full `}
         >
-          <FaCalculator
-            className={
-              isLinkActive("/entry-management") ? "w-9 h-9" : "w-6 h-6"
-            }
-          ></FaCalculator>
+          <FaCalculator className="w-8 h-8"></FaCalculator>
           <span className="ml-6">Entry Management</span>
         </div>
       </Link>
 
       <Link href="/account-management">
         <div
-          className={`flex pl-8 py-8 focus:outline-none ${
+          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
             isLinkActive("/account-management")
-              ? "bg-gray-700 font-black  text-xl"
-              : "font-bold"
-          } hover:underline underline-offset-8`}
+              ? "bg-slate-600 hover:bg-slate-600"
+              : ""
+          } rounded-full`}
         >
-          <MdOutlineAccountBalance
-            className={
-              isLinkActive("/account-management") ? "w-9 h-9" : "w-6 h-6"
-            }
-          ></MdOutlineAccountBalance>
+          <MdOutlineAccountBalance className="w-8 h-8"></MdOutlineAccountBalance>
           <span className="ml-4">Account Management</span>
         </div>
       </Link>
 
       <Link href="/users">
         <div
-          className={`flex pl-8 py-8 focus:outline-none ${
-            isLinkActive("/users")
-              ? "bg-gray-700 font-black  text-xl"
-              : "font-bold"
-          }  hover:underline underline-offset-8`}
+          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
+            isLinkActive("/users") ? "bg-slate-600 hover:bg-slate-600" : ""
+          } rounded-full `}
         >
-          <FaUserFriends
-            className={isLinkActive("/users") ? "w-9 h-9" : "w-6 h-6"}
-          ></FaUserFriends>
+          <FaUserFriends className="w-8 h-8"></FaUserFriends>
           <span className="ml-16">Users</span>
         </div>
       </Link>
 
+      <Separator className="ml-9 my-7" />
+
       <Link href="/">
-        <div className="flex pl-8 py-8 focus:outline-none focus:bg-gray-700 ">
+        <div className="flex pl-5 py-4 focus:outline-none hover:bg-slate-500 rounded-full">
           <MdArrowBack className="w-8 h-8"></MdArrowBack>
-          <span className="ml-8 font-medium">Return to login</span>
+          <span className="ml-8 font-medium ">Back to Login</span>
         </div>
       </Link>
     </div>
