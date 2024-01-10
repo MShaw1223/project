44e85302ced9e262cdccc7eb84ee11cc4f5c796d
miller <millershaw61@gmail.com>
2024-01-10 17:00:50 +0000

    commit

--- src/components/ui/menu.tsx
+++ src/components/ui/menu.tsx
@@ -10,77 +10,77 @@ export default function Menu() {
   const router = useRouter();
   const isLinkActive = (href: string) => router.pathname === href;
   return (
-    <div className="w-72 h-screen bg-slate-400">
-      <div className="text-center p-5 text-2xl font-sans font-semibold">
+    <div className="w-[300px] h-screen bg-black">
+      <div className="text-center p-5 text-2xl font-sans font-semibold text-slate-100">
         Menu
       </div>
       <Link href="/home">
         <div
-          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
+          className={`flex pl-5 py-4 mx-3 focus:outline-none font-bold hover:bg-slate-500 ${
             isLinkActive("/home") ? "bg-slate-600 hover:bg-slate-600" : ""
           } rounded-full `}
         >
-          <FaHome className="w-6 h-6"></FaHome>
-          <span className="pl-3">Home</span>
+          <FaHome className="w-6 h-6 text-slate-100"></FaHome>
+          <span className="pl-3 text-slate-100">Home</span>
         </div>
       </Link>
 
       <Link href="/Trade-Entry">
         <div
-          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
+          className={`flex pl-5 py-4 mx-3 focus:outline-none font-bold hover:bg-slate-500 ${
             isLinkActive("/Trade-Entry")
               ? "bg-slate-600 hover:bg-slate-600"
               : ""
           } rounded-full `}
         >
-          <BsFillJournalBookmarkFill className="w-6 h-6"></BsFillJournalBookmarkFill>
-          <span className="pl-3">Trade Entry</span>
+          <BsFillJournalBookmarkFill className="w-6 h-6 text-slate-100"></BsFillJournalBookmarkFill>
+          <span className="pl-3 text-slate-100">Trade Entry</span>
         </div>
       </Link>
 
       <Link href="/entry-management">
         <div
-          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
+          className={`flex pl-5 py-4 mx-3 focus:outline-none font-bold hover:bg-slate-500 ${
             isLinkActive("/entry-management")
               ? "bg-slate-600 hover:bg-slate-600"
               : ""
           } rounded-full `}
         >
-          <FaCalculator className="w-6 h-6"></FaCalculator>
-          <span className="pl-3">Entry Management</span>
+          <FaCalculator className="w-6 h-6 text-slate-100"></FaCalculator>
+          <span className="pl-3 text-slate-100">Entry Management</span>
         </div>
       </Link>
 
       <Link href="/account-management">
         <div
-          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
+          className={`flex pl-5 py-4 mx-3 focus:outline-none font-bold hover:bg-slate-500 ${
             isLinkActive("/account-management")
               ? "bg-slate-600 hover:bg-slate-600"
               : ""
           } rounded-full`}
         >
-          <MdOutlineAccountBalance className="w-6 h-6"></MdOutlineAccountBalance>
-          <span className="pl-3">Account Management</span>
+          <MdOutlineAccountBalance className="w-6 h-6 text-slate-100"></MdOutlineAccountBalance>
+          <span className="pl-3 text-slate-100">Account Management</span>
         </div>
       </Link>
 
       <Link href="/users">
         <div
-          className={`flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 ${
-            isLinkActive("/users") ? "bg-slate-600 hover:bg-slate-600" : ""
+          className={`flex pl-5 py-4 mx-3 focus:outline-none font-bold hover:bg-slate-500 ${
+            isLinkActive("/users") ? "bg-gray-700 hover:bg-gray-700" : ""
           } rounded-full `}
         >
-          <FaUserFriends className="w-6 h-6"></FaUserFriends>
-          <span className="pl-3">Users</span>
+          <FaUserFriends className="w-6 h-6 text-slate-100"></FaUserFriends>
+          <span className="pl-3 text-slate-100">Users</span>
         </div>
       </Link>
 
-      <Separator className="ml-9 my-7" />
+      <Separator className="ml-9 my-7 bg-white" />
 
       <Link href="/">
-        <div className="flex pl-5 py-4 focus:outline-none font-bold hover:bg-slate-500 rounded-full">
-          <MdArrowBack className="w-6 h-6"></MdArrowBack>
-          <span className="pl-3">Back to Login</span>
+        <div className="flex pl-5 py-4 mx-3 focus:outline-none font-bold hover:bg-slate-500 rounded-full">
+          <MdArrowBack className="w-6 h-6 text-slate-100"></MdArrowBack>
+          <span className="pl-3 text-slate-100">Back to Login</span>
         </div>
       </Link>
     </div>
