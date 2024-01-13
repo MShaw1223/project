e3cc00f4c0f5b66dc62a2174b254abc4b07ba149
MShaw1223 <134694154+MShaw1223@users.noreply.github.com>
2024-01-13 14:56:25 +0000

    commit

--- src/pages/Trade-Entry.tsx
+++ src/pages/Trade-Entry.tsx
@@ -116,9 +116,7 @@ const tradeEntry: NextPage = () => {
                       ></AccountDropdown>
                     </div>
                   </div>
-                  <div className="flex flex-col m-2 bg-black">
                     <Separator orientation="vertical" />
-                  </div>
                   <div className="flex flex-col items-center w-4/5">
                     <form onSubmit={handleSubmit}>
                       <div className="p-3 w-full">
