17b7245b292d1b197db99b8aaefa45b42b85f336
MShaw1223 <134694154+MShaw1223@users.noreply.github.com>
2024-01-13 14:25:41 +0000

    trying something

--- src/pages/Trade-Entry.tsx
+++ src/pages/Trade-Entry.tsx
@@ -116,9 +116,7 @@ const tradeEntry: NextPage = () => {
                       ></AccountDropdown>
                     </div>
                   </div>
-                  <div className="flex flex-col m-4">
-                    <Separator orientation="vertical" />
-                  </div>
+                    <Separator orientation="vertical flex flex-col" />
                   <div className="flex flex-col items-center w-4/5">
                     <form onSubmit={handleSubmit}>
                       <div className="p-3 w-full">
