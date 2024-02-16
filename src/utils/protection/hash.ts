1a736197b8cabad3a2a262752efec893d8626988
MShaw1223 <134694154+MShaw1223@users.noreply.github.com>
2024-02-16 18:24:39 +0000

    update: comments

--- src/utils/protection/hash.ts
+++ src/utils/protection/hash.ts
@@ -2,7 +2,7 @@ export function generateKey(username: string): string {
   let hash = 0;
   for (let i = 0; i < username.length; i++) {
     const char = username.charCodeAt(i); // converts to unicode
-    hash = (hash << 5) - hash + char;
+    hash = (hash << 5) - hash + char; //left shift the hash 5 places then - hash + unicode of username
     hash &= hash; // Convert to 32-bit integer
   }
   return hash.toString(16);
