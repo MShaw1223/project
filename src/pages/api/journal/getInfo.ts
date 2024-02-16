9a171d2d2a98680c198d40f6f37828400fe34a0e
MShaw1223 <134694154+MShaw1223@users.noreply.github.com>
2024-02-16 09:06:34 +0000

    getinfo journal API changes

--- src/pages/api/journal/getInfo.ts
+++ src/pages/api/journal/getInfo.ts
@@ -1,6 +1,7 @@
 // import type { NextApiRequest, NextApiResponse } from "next";
 import { Pool } from "@neondatabase/serverless";
 import sqlstring from "sqlstring";
+import { useRouter } from "next/router";
 //retrieval(variable){
 // etc etc
 //  }
@@ -8,9 +9,39 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+
+//const { li } = router.query;
+// const loggedIn = li; 
 // where tradesid = ${loggedIn} AND
 
 export default async function retreival(selectedAccount: any) {
+  const router = useRouter(); 
+  const { li } = router.query;
+  console.log(li);
+  if (li !== undefined) {
+    try {
+      const user = await fetch("/api/auth/userFromHash", {
+        method: "POST",
+        body: JSON.stringify(li),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const lgdin = await user.json();
+      console.log("User logged in: ", lgdin);
+      return lgdin;
+    }catch(error){
+      console.error("Error fetching user: ", error);
+    }
+  }
+  try {
+    const ID = await fetch("", {
+      method: "POST",
+      body: lgdin,
+      headers: {"Content-Type": "application/json"}
+    })
+  } catch (error) {
+    console.error("Error fetching ID: ", error);
+  }
   try {
     const getTradesInfo = sqlstring.format(
       `
