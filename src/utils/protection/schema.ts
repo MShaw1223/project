1a416d264d75bef19a1a4399bf9c1ba7b7afd8af
Miller Shaw <134694154+MShaw1223@users.noreply.github.com>
2024-02-28 13:48:06 +0000

    Update schema.ts

--- src/utils/protection/schema.ts
+++ src/utils/protection/schema.ts
@@ -64,7 +64,10 @@ const userPwdSchema = zod.object({
   username: zod.string(),
   passwd: zod.string(),
 });
-
+const lginSignUpSchema = = zod.object({
+  passwd: zod.string().max(60),
+  username: zod.string().max(15),
+});
 export {
   deleteAccountSchema,
   deleteEntrySchema,
@@ -72,4 +75,5 @@ export {
   newAccountSchema,
   newCurrencySchema,
   userPwdSchema,
+  lginSignUpSchema,
 };
