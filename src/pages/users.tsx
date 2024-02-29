b055a3193aef598c158b84a79f6997ae393df0d6
Miller Shaw <134694154+MShaw1223@users.noreply.github.com>
2024-02-29 22:20:30 +0000

    Update users.tsx

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -2,8 +2,6 @@ import { NextPage } from "next";
 import { FaUserFriends } from "react-icons/fa";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Menu from "@/utils/menu";
-import DeleteUser from "@/utils/users/DeleteUser";
-import EditUser from "@/utils/users/EditUser";
 import * as React from "react";
 import withAuth from "@/utils/protection/authorise";
 
@@ -22,18 +20,7 @@ const userPage: NextPage = () => {
           <span className="ml-16 font-bold">Users</span>
         </div>
         <div className="flex-1 overflow-auto justify-center p-2">
-          <Tabs defaultValue="edit" className="p-3 m-1">
-            <TabsList className="m-2 mb-6">
-              <TabsTrigger value="edit">Edit User</TabsTrigger>
-              <TabsTrigger value="delete">Delete User</TabsTrigger>
-            </TabsList>
-            <TabsContent value="edit">
-              <EditUser />
-            </TabsContent>
-            <TabsContent value="delete">
-              <DeleteUser />
-            </TabsContent>
-          </Tabs>
+          
         </div>
       </div>
     </div>
