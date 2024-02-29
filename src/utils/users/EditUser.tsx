5658c124739c29c314a1dc691d116b48e61f185f
Miller Shaw <134694154+MShaw1223@users.noreply.github.com>
2024-02-29 22:24:34 +0000

    commit

--- src/utils/users/EditUser.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
-import { FormEvent } from "react";
-
-//interface finds user and shows a pop-up to edit
-//shadcn has a really nice pop-up for this
-// put is correct http req for this look at how they work
-
-
