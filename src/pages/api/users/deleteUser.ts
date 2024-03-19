56bb26b4e6fbde93831d946b32634f4120827189
miller <millershaw61@gmail.com>
2024-03-19 22:32:26 +0000

    delete user working added optional chaining instead of a complex sql query

--- src/pages/api/users/deleteUser.ts
+++ src/pages/api/users/deleteUser.ts
@@ -19,6 +19,7 @@ export default async function delUser(req: NextApiRequest) {
         connectionString: process.env.DATABASE_URL,
       });
       const body = await extractBody(req);
+      console.log("body: ", body);
       const { userID } = body;
       console.log("userID: ", userID);
       const accountIDquery = sqlstring.format(
@@ -26,33 +27,43 @@ export default async function delUser(req: NextApiRequest) {
         [userID]
       );
       const accountsIDResult = await pool.query(accountIDquery);
-      const accountID = accountsIDResult.rows[0].accountid;
-      const tblTradesSql = sqlstring.format(
-        "DELETE from tableTrades where accountid = ?;",
-        [accountID]
-      );
+      const accountID = accountsIDResult.rows[0]?.accountid; // optional chaining for if no accountID
       const usersTableSql = sqlstring.format(
-        "DELETE tableUsers WHERE userID = ?;",
+        "DELETE from tableUsers WHERE userid = ?;",
         [userID]
       );
       const accountsTableSql = sqlstring.format(
         "DELETE from tableAccounts WHERE userID = ?;",
         [userID]
       );
-      const pairsTableSql = sqlstring.format(
-        "DELETE from tablePairs WHERE userID = ?;",
-        [userID]
-      );
+      const tblTradesSql = accountID
+        ? sqlstring.format("DELETE FROM tableTrades WHERE accountid = ?;", [
+            accountID,
+          ])
+        : null; // If there's no accountID, set tblTradesSql to null
+      const pairsTableSql = accountID
+        ? sqlstring.format("DELETE FROM tablePairs WHERE accountid = ?;", [
+            accountID,
+          ])
+        : null; // If there's no accountID, set pairsTableSql to null
+
       await pool.query(usersTableSql);
-      await pool.query(pairsTableSql);
       await pool.query(accountsTableSql);
-      await pool.query(tblTradesSql);
+      if (tblTradesSql) {
+        await pool.query(tblTradesSql);
+        console.log("sql tblTrades: ", tblTradesSql);
+      }
+      if (pairsTableSql) {
+        await pool.query(pairsTableSql);
+        console.log("sql tblPairs: ", pairsTableSql);
+      }
       await pool.end();
       console.log("sql tblUsers: ");
       console.log("sql tblTrades: ", tblTradesSql);
       return NextResponse.json("Deleted user", { status: 200 });
     } catch (error) {
       console.error("Error deleting user: ", error);
+      console.error("Stack trace: ", (error as Error).stack);
       return NextResponse.json({ error: "Bad request" }, { status: 400 });
     }
   }
--- src/utils/users/DeleteUser.tsx
+++ src/utils/users/DeleteUser.tsx
@@ -1,59 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, useEffect } from "react";
 import { useMutation } from "react-query";
 
 const DeleteUserPage: NextPage = () => {
   const router = useRouter();
   const [ID, setID] = useState<string>("");
-  async function getuserID() {
-    const { li: loggedInVal } = router.query;
-    console.log("Li: ", loggedInVal);
-    if (loggedInVal !== undefined) {
-      const getuserID = await fetch("/api/auth/IDFromHash", {
-        method: "POST",
-        body: JSON.stringify(loggedInVal),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const foundID = await getuserID.json();
-      setID(foundID);
-    }
-  }
   const mutation = useMutation({
-    //formdata is likely the root cause? if not look at how id is handled front and backend
-//     Error deleting user:   error: invalid input syntax for type integer: ""
-//     at (node_modules/@neondatabase/serverless/index.js:1340:61)
-//     at (src/pages/api/users/deleteUser.ts:28:31)
-//     at (node_modules/next/dist/esm/server/web/adapter.js:157:0) {
-//   length: 105,
-//   name: 'error',
-//   severity: 'ERROR',
-//   code: '22P02',
-//   detail: undefined,
-//   hint: undefined,
-//   position: '52',
-//   internalPosition: undefined,
-//   internalQuery: undefined,
-//   where: undefined,
-//   schema: undefined,
-//   table: undefined,
-//   column: undefined,
-//   dataType: undefined,
-//   constraint: undefined,
-//   file: 'numutils.c',
-//   line: '232',
-//   routine: 'pg_strtoint32'
-// }
-// userid is also in default state of ""
     mutationFn: async (formData: string) => {
       console.log("Handle Submit works");
+      console.log("formdata: ", formData);
       const done = await fetch("/api/users/deleteUser", {
         method: "DELETE",
         body: formData,
       });
+      console.log("Response status:", done?.status);
       if (!done.ok) {
         const errMessage = await done.text();
         throw new Error(errMessage);
@@ -67,10 +29,27 @@ const DeleteUserPage: NextPage = () => {
       console.error("Mutation error:", error);
     },
   });
+  useEffect(() => {
+    async function getuserID() {
+      const { li: loggedInVal } = router.query;
+      console.log("Li: ", loggedInVal);
+      if (loggedInVal !== undefined) {
+        const getuserID = await fetch("/api/auth/IDFromHash", {
+          method: "POST",
+          body: JSON.stringify(loggedInVal),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const foundID = await getuserID.json();
+        setID(foundID);
+      }
+    }
+    getuserID();
+  }, []);
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    await getuserID();
     try {
       mutation.mutate(
         JSON.stringify({
