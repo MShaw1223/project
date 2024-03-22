8faa5d549e0e97c4d430ed32001de1d14662f5bf
Miller Shaw <134694154+MShaw1223@users.noreply.github.com>
2024-03-22 17:58:00 +0000

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head />
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>NEA Project | Miller Shaw</title>
+        <title>FXTrax</title>
       </head>
       <body>
         <Main />
