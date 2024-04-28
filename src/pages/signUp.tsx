038d57383c0613ee553639529c9373542b32bdc8
Miller Shaw <134694154+MShaw1223@users.noreply.github.com>
2024-04-28 14:47:59 +0100

    clean commenting

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -8,27 +8,19 @@ import { keyGenerator } from "@/utils/hash";
 import { lginSignUpSchema } from "@/utils/schema";
 import { BsChevronBarLeft } from "react-icons/bs";
 import Link from "next/link";
-
 const signUp: NextPage = () => {
-  // determines if the password is hidden or not when entering it during login
   const [showPassword, setShowPassword] = React.useState(false);
-  // used for pushing between different pages, in this case to the home page
   const router = useRouter();
-  // when user presses the 'Go !' button it triggers the handleSubmit function
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     // prevents the page from refreshing during the process
     event.preventDefault();
-    // gets all the information from the form
     const data = new FormData(event.target as HTMLFormElement);
     const confirmPasswd = data.get("confirmPassword") as string;
     const parsedData = lginSignUpSchema.parse({
       passwd: data.get("firstPassword") as string,
       username: data.get("user") as string,
     });
-
-    // the if statement checks the password has been entered correctly before submitting the parsedData object
-    // to the api to be submitted to the Neon database
-    // ( parseData containing the future login credentials of username and passwd )
+    // the if statement checks the password has been entered correctly before submitting the parsedData object to the api to be submitted to the Neon database
     if (parsedData.passwd === confirmPasswd) {
       const response = await fetch("/api/auth/userPwd", {
         method: "POST",
@@ -39,9 +31,7 @@ const signUp: NextPage = () => {
         cache: "no-store",
       });
       if (response.ok) {
-        // if the response returns a 200,
-        // the keyGenerator function takes the username entered above as a param
-        // to generate a key  the key will be used as a logged in identifier
+        // if the response returns a 200, the keyGenerator function takes the username entered above as a param to generate a key  the key will be used as a logged in identifier
         const { username } = parsedData;
         const key = keyGenerator(username);
         router.push(`/home?li=${key}`);
@@ -117,5 +107,4 @@ const signUp: NextPage = () => {
     </>
   );
 };
-
 export default signUp;
